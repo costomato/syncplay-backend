bac8dd99ee3d0320fba93000c03ba5ca9b355bba
Kaustubh <sherlockaustubh@gmail.com>
2023-02-03 23:00:13 +0530

    added dockerfile

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,8 @@ plugins {
 group = "com.flyprosper"
 version = "0.0.1"
 application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
+//    mainClass.set("io.ktor.server.netty.EngineMain")
+    mainClass.set("com.flyprosper.ApplicationKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
